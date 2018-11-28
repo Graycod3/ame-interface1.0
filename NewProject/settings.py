661b61e9e930bba69a77d4c602877285cfc454d7
Graycod3 <bngorima2@gmail.com>
2018-11-28 09:27:12 +0000

    HTML for site.

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=cbgkdma9bxy+0kx-$wk=l5d&*-kr7ou0%tbgsj9a%(=vlg*z('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.37', '192.168.1.38', '127.0.0.1', '.pythonanywhere.com', 'localhost']
 
